bfd968e4b16aaea77a143ee27be05c8417c6fc63
Karolina Woloszyn <karolina.woloszyn@gmail.com>
2018-01-22 18:25:36 +0100

    First version of generatoremail app

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+    Email em1 = new Email("Adam", "Nowak");
+    em1.showInfo();
+    System.out.println(em1.showInfo());
+	}
+
+}
